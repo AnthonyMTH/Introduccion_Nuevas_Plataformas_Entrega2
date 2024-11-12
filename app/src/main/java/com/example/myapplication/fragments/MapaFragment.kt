a74efd523211c8d456aede1745263c0900b18d99
AnthonyMTH <aticonaha@unsa.edu.pe>
2024-11-11 21:24:14 -0500

    Se muestra mapa con las edificaciones (google maps api)

--- app/src/main/java/com/example/myapplication/fragments/MapaFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/MapaFragment.kt
@@ -8,81 +8,55 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import com.example.myapplication.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MapaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MapaFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mapa, container, false)
-    }
+class MapaFragment : Fragment(R.layout.fragment_mapa), OnMapReadyCallback {
+    private var map: GoogleMap? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        // Referencia al AutoCompleteTextView
-        val autoCompleteTextView = view.findViewById<AutoCompleteTextView>(R.id.auto_complete_txt)
-
-        // Lista de opciones para el autocompletado
-        val edificaciones = listOf("Iglesia San Francisco", "Museo Histórico", "Casona Colonial", "Monasterio Santa Catalina")
-
-        // Configuración del adaptador para el autocompletado
-        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, edificaciones)
-        autoCompleteTextView.setAdapter(adapter)
-
-        // Mostrar el menú desplegable al hacer clic
-        autoCompleteTextView.setOnClickListener {
-            autoCompleteTextView.showDropDown()
-        }
-
-        // Manejar la selección de un elemento para mostrarlo en el campo
-        autoCompleteTextView.setOnItemClickListener { parent, _, position, _ ->
-            val selectedItem = parent.getItemAtPosition(position) as String
-            autoCompleteTextView.setText(selectedItem, false) // Muestra el elemento seleccionado en el campo
-        }
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
     }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MapaFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MapaFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        // centro del mapa en Arequipa
+        val arequipa = LatLng(-16.409047, -71.537451)
+        map?.moveCamera(CameraUpdateFactory.newLatLngZoom(arequipa, 12f))
+
+        // marcadores para las ubicaciones
+        addMarkers()
+    }
+
+    private fun addMarkers() {
+        val locations = listOf(
+            LatLng(-16.398803, -71.536865) to "Catedral de Arequipa",
+            LatLng(-16.395878, -71.533785) to "Monasterio de Santa Catalina",
+            LatLng(-16.398360, -71.536215) to "Iglesia de la Compañía de Jesús",
+            LatLng(-16.401229, -71.535762) to "Casa Tristán del Pozo",
+            LatLng(-16.404678, -71.534474) to "Monasterio de La Recoleta",
+            LatLng(-16.401897, -71.537491) to "Casa del Moral",
+            LatLng(-16.404617, -71.533947) to "Iglesia de San Francisco",
+            LatLng(-16.405124, -71.535263) to "Iglesia de Santo Domingo",
+            LatLng(-16.422939, -71.524669) to "Molino de Sabandía",
+            LatLng(-16.400691, -71.537683) to "Museo Santuarios Andinos"
+        )
+
+        for ((location, title) in locations) {
+            map?.addMarker(MarkerOptions().position(location).title(title))
+        }
+
+        // evento al hacer clic en el marcador, se muestra nombre
+        map?.setOnMarkerClickListener { marker ->
+            marker.showInfoWindow()
+            true
+        }
     }
 }
