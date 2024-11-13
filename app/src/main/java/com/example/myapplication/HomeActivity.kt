ca267f29766a887c3a681e506a3d70870050b922
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2024-11-13 14:19:29 -0500

    add layout home login register

---
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.myapplication.fragments.HomeFragment
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val edificationListFragment: Fragment = HomeFragment()
+        openFragment(edificationListFragment)
+    }
+    private fun openFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+}
